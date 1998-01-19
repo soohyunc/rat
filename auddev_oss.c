5fe5be252ad4e0810af562977e42f4e048d71942
ucaccsp <ucaccsp@bc8abe95-590b-0410-a0f3-dc9ed9a1a8fa>
1998-01-19 19:34:20 +0000

    Set fragment size dynamically, depending on sample rate

--- auddev_oss.c
+++ auddev_oss.c
@@ -65,16 +65,21 @@ audio_format format;
 int 
 audio_open_rw(char rw)
 {
-	int mode     = AFMT_S16_LE;			/* 16bit linear, little-endian */
-	int stereo   = format.num_channels - 1;		/* 0=mono, 1=stereo            */
-	int speed    = format.sample_rate;
-	int volume   = (100<<8)|100;
-#ifdef NDEF
-	int frag     = 0x7fff0007; 			/* unlimited number of 128 byte fragments */
-#endif
-	int reclb    = 0;
-	int audio_fd = -1;
+	int  mode     = AFMT_S16_LE;			/* 16bit linear, little-endian */
+	int  stereo   = format.num_channels - 1;	/* 0=mono, 1=stereo            */
+	int  speed    = format.sample_rate;
+	int  volume   = (100<<8)|100;
+	int  frag     = 0x7fff0000; 			/* unlimited number of fragments */
+	int  fragsize = 0;				/* temporary, used to calculate frag */
+	int  reclb    = 0;
+	int  audio_fd = -1;
 	char buffer[128];				/* sigh. */
+
+	/* Calculate the size of the fragments we want... 20ms worth of audio data... */
+	fragsize = DEVICE_BUF_UNIT * (format.sample_rate / 8000) * (format.bits_per_sample / 8);
+	/* Round to the nearest legal frag size (next power of two lower...) */
+	frag |= (int) (log(fragsize)/log(2));
+	printf("frag=%d\n", (int) (log(fragsize)/log(2)));
 
 	switch (rw) {
 	case O_RDONLY: 
@@ -104,12 +109,10 @@ audio_open_rw(char rw)
 			printf("       in this case, so this error should never happen......\n");
 			exit(1);
 		}
-#ifdef NDEF
 		if ((ioctl(audio_fd, SNDCTL_DSP_SETFRAGMENT, &frag) == -1)) {
 			printf("ERROR: Cannot set the fragement size\n");
 			exit(1);
 		}
-#endif
 		if ((ioctl(audio_fd, SNDCTL_DSP_SETFMT, &mode) == -1) || (mode != AFMT_S16_LE)) { 
 			printf("ERROR: Audio device doesn't support 16bit linear format!\n");
 			exit(1);
