f3230c68f0495163911f5405faeaff6119f042c0
mbiken <mbiken@bc8abe95-590b-0410-a0f3-dc9ed9a1a8fa>
1998-09-30 17:46:04 +0000

    Quick hacky fix to the packet overflow bug in the mbus code... make the buffer bigger! :-) [csp]

--- mbus.c
+++ mbus.c
@@ -44,7 +44,7 @@
 #include "net.h"
 #include "mbus.h"
 
-#define MBUS_BUF_SIZE	1024
+#define MBUS_BUF_SIZE	65535		/* FIXME: should be < 1500 */
 #define MBUS_MAX_ADDR	10
 #define MBUS_MAX_PD	10
 #define MBUS_MAX_QLEN	50		/* Number of messages we can queue with mbus_qmsg() */
