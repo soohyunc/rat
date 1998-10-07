3f55727a17f2a16e5a4cfc52da1665c0a25f2112
ucaccsp <ucaccsp@bc8abe95-590b-0410-a0f3-dc9ed9a1a8fa>
1998-10-07 23:51:32 +0000

    - Convert IPv4 code to use a "struct in_addr" instead of a uint_32 to hold   the address and to use inet_aton() instead of inet_addr(). - Add IPv6 code (untested).

--- net_udp.c
+++ net_udp.c
@@ -36,6 +36,11 @@
  * SUCH DAMAGE.
  */
 
+/* If this machine supports IPv6 the symbol HAVE_IPv6 should */
+/* be defined in either config_unix.h or config_win32.h. The */
+/* appropriate system header files should also be included   */
+/* by those files.                                           */
+
 #include "assert.h"
 #include "config_unix.h"
 #include "config_win32.h"
@@ -46,16 +51,15 @@
 #define IPv6	6
 
 struct _socket_udp {
-	int	 mode;	/* IPv4 or IPv6 */
-	char	*addr;
-	u_int16	 port;
-#ifdef WIN32
-	int	 ttl;
-#else
-	char	 ttl;
+	int	 	 mode;	/* IPv4 or IPv6 */
+	char		*addr;
+	u_int16	 	 port;
+	ttl_t	 	 ttl;
+	fd_t	 	 fd;
+	struct in_addr	 addr4;
+#ifdef HAVE_IPv6
+	struct in6_addr	 addr6;
 #endif
-	u_int32	 addr4;
-	fd_t	 fd;
 };
 
 static void
@@ -83,8 +87,7 @@ static socket_udp *udp_init4(char *addr, int port, int ttl)
 	s->addr  = addr;
 	s->port  = port;
 	s->ttl   = ttl;
-	s->addr4 = inet_addr(addr);
-	if (s->addr4 == 0xffffffff) {
+	if (!inet_aton(addr, &s->addr4)) {
 		struct hostent *h = gethostbyname(addr);
 		if (h == NULL) {
 			return NULL;
@@ -113,11 +116,11 @@ static socket_udp *udp_init4(char *addr, int port, int ttl)
 		socket_error("bind");
 		abort();
 	}
-	if (IN_MULTICAST(ntohl(s->addr4))) {
+	if (IN_MULTICAST(ntohl(s->addr4.s_addr))) {
 		char            loop = 1;
 		struct ip_mreq  imr;
 
-		imr.imr_multiaddr.s_addr = s->addr4;
+		imr.imr_multiaddr        = s->addr4;
 		imr.imr_interface.s_addr = htonl(INADDR_ANY);
 
 		if (setsockopt(s->fd, IPPROTO_IP, IP_ADD_MEMBERSHIP, (char *) &imr, sizeof(struct ip_mreq)) != 0) {
@@ -148,40 +151,13 @@ static int udp_send4(socket_udp *s, char *buffer, int buflen)
 	assert(buffer != NULL);
 	assert(buflen > 0);
 
-	memcpy((char *) &s_in.sin_addr.s_addr, (char *) &(s->addr4), sizeof(s->addr4));
 	s_in.sin_family = AF_INET;
+	s_in.sin_addr   = s->addr4;
 	s_in.sin_port   = htons(s->port);
 	if ((ret = sendto(s->fd, buffer, buflen, 0, (struct sockaddr *) &s_in, sizeof(s_in))) < 0) {
 		socket_error("udp_send4");
 	}
 	return ret;
-}
-
-static int udp_recv4(socket_udp *s, char *buffer, int buflen)
-{
-	/* Reads data into the buffer, returning the number of bytes read.   */
-	/* If no data is available, this returns the value zero immediately. */
-	fd_set		rfd;
-	struct timeval	t;
-	int		len;
-
-	assert(s != NULL);
-	assert(s->mode == IPv4);
-	assert(buffer != NULL);
-	assert(buflen > 0);
-
-	t.tv_sec  = 0;
-	t.tv_usec = 0;
-	FD_ZERO(&rfd);
-	FD_SET(s->fd, &rfd);
-	if (select(s->fd + 1, &rfd, NULL, NULL, &t) > 0) {
-		len = recvfrom(s->fd, buffer, buflen, 0, 0, 0);
-		if (len > 0) {
-			return len;
-		}
-		socket_error("recvfrom");
-	}
-	return 0;
 }
 
 /*****************************************************************************/
@@ -190,25 +166,92 @@ static int udp_recv4(socket_udp *s, char *buffer, int buflen)
 
 static socket_udp *udp_init6(char *addr, int port, int ttl)
 {
+#ifdef HAVE_IPv6
+	int                 reuse = 1;
+	struct sockaddr_in6 s_in;
+	socket_udp         *s = (socket_udp *) malloc(sizeof(socket_udp));
+	s->mode  = IPv6;
+	s->addr  = addr;
+	s->port  = port;
+	s->ttl   = ttl;
+	if (!inet_pton(AF_INET6, addr, &s->addr6)) {
+		/* We should probably try to do a DNS lookup on the name */
+		/* here, but I'm trying to get the basics going first... */
+		return NULL;	
+	}
+	s->fd = socket(AF_INET6, SOCK_DGRAM, 0);
+	if (s->fd < 0) {
+		socket_error("socket");
+		abort();
+	}
+	if (setsockopt(s->fd, SOL_SOCKET, SO_REUSEADDR, (char *) &reuse, sizeof(reuse)) != 0) {
+		socket_error("setsockopt SO_REUSEADDR");
+		abort();
+	}
+	if (setsockopt(s->fd, SOL_SOCKET, SO_REUSEPORT, (char *) &reuse, sizeof(reuse)) != 0) {
+		socket_error("setsockopt SO_REUSEPORT");
+		abort();
+	}
+	s_in.sin6_family       = AF_INET6;
+	s_in.sin6_addr.s6_addr = s->addr6;
+	s_in.sin6_port         = htons(port);
+	if (bind(s->fd, (struct sockaddr *) &s_in, sizeof(s_in)) != 0) {
+		socket_error("bind");
+		abort();
+	}
+	if (IN6_IS_ADDR_MULTICAST(ntohl(s->addr6.s6_addr))) {
+		char              loop = 1;
+		struct ipv6_mreq  imr;
+
+		imr.ipv6mr_multiaddr = s->addr6;
+		imr.ipv6mr_interface = 0;
+
+		if (setsockopt(s->fd, IPPROTO_IP6, IPV6_ADD_MEMBERSHIP, (char *) &imr, sizeof(struct ip_mreq)) != 0) {
+			socket_error("setsockopt IPV6_ADD_MEMBERSHIP");
+			abort();
+		}
+		if (setsockopt(s->fd, IPPROTO_IP6, IPV6_MULTICAST_LOOP, &loop, sizeof(loop)) != 0) {
+			socket_error("setsockopt IPV6_MULTICAST_LOOP");
+			abort();
+		}
+		if (setsockopt(s->fd, IPPROTO_IP6, IPV6_MULTICAST_HOPS, (char *) &s->ttl, sizeof(s->ttl)) != 0) {
+			socket_error("setsockopt IPV6_MULTICAST_HOPS");
+			abort();
+		}
+	}
+	return s;
+#else
+	UNUSED(addr);
+	UNUSED(port);
+	UNUSED(ttl);
 	return NULL;
+#endif
 }
 
 static int udp_send6(socket_udp *s, char *buffer, int buflen)
 {
-	assert(s != NULL);
-	assert(s->mode == IPv6);
-	assert(buffer != NULL);
-	assert(buflen > 0);
-	return -1;
-}
+#ifdef HAVE_IPv6
+	struct sockaddr_in6	s_in;
+	int			ret;
 
-static int udp_recv6(socket_udp *s, char *buffer, int buflen)
-{
 	assert(s != NULL);
 	assert(s->mode == IPv6);
 	assert(buffer != NULL);
 	assert(buflen > 0);
+
+	s_in.sin6_family = AF_INET6;
+	s_in.sin6_addr   = s->addr6;
+	s_in.sin6_port   = htons(s->port);
+	if ((ret = sendto(s->fd, buffer, buflen, 0, (struct sockaddr *) &s_in, sizeof(s_in))) < 0) {
+		socket_error("udp_send6");
+	}
+	return ret;
+#else
+	UNUSED(s);
+	UNUSED(buffer);
+	UNUSED(buflen);
 	return -1;
+#endif
 }
 
 /*****************************************************************************/
@@ -239,11 +282,28 @@ int udp_send(socket_udp *s, char *buffer, int buflen)
 
 int udp_recv(socket_udp *s, char *buffer, int buflen)
 {
-	switch (s->mode) {
-		case IPv4 : return udp_recv4(s, buffer, buflen);
-		case IPv6 : return udp_recv6(s, buffer, buflen);
-		default   : abort();
+	/* Reads data into the buffer, returning the number of bytes read.   */
+	/* If no data is available, this returns the value zero immediately. */
+	/* Note: since we don't care about the source address of the packet  */
+	/* we receive, this function becomes protocol independent.           */
+	fd_set		rfd;
+	struct timeval	t;
+	int		len;
+
+	assert(buffer != NULL);
+	assert(buflen > 0);
+
+	t.tv_sec  = 0;
+	t.tv_usec = 0;
+	FD_ZERO(&rfd);
+	FD_SET(s->fd, &rfd);
+	if (select(s->fd + 1, &rfd, NULL, NULL, &t) > 0) {
+		len = recvfrom(s->fd, buffer, buflen, 0, 0, 0);
+		if (len > 0) {
+			return len;
+		}
+		socket_error("recvfrom");
 	}
-	return -1;
+	return 0;
 }
 
