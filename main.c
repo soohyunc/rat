f9ed327e77929652c1e28490141a2f103546108d
ucackha <ucackha@bc8abe95-590b-0410-a0f3-dc9ed9a1a8fa>
1998-10-14 00:20:28 +0000

    re-added deleted code ...tcl/tk on windows uses unsigned pids.  Kris

--- main.c
+++ main.c
@@ -140,8 +140,13 @@ main(int argc, char *argv[])
         audio_set_interface(0);
         converters_init();
 
+#ifdef WIN32
+	sprintf(mbus_engine_addr, "(audio engine rat %lu)", (u_int32) getpid());
+	sprintf(mbus_ui_addr,	  "(audio     ui rat %lu)", (u_int32) getpid());
+#else
 	sprintf(mbus_engine_addr, "(audio engine rat %ld)", (int32) getpid());
 	sprintf(mbus_ui_addr,     "(audio     ui rat %ld)", (int32) getpid());
+#endif
 	sprintf(mbus_video_addr,  "(video engine   *  *)");
 	
 	sp[0]->mbus_engine_base = mbus_init(0, mbus_engine_rx, NULL); 
