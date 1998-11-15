0bbd82559119d3cb8a9af6676cfa1599f66298d1
ucacoxh <ucacoxh@bc8abe95-590b-0410-a0f3-dc9ed9a1a8fa>
1998-11-15 11:29:41 +0000

    - Removed misleading comment on energy calc. Put better one in :-)

--- parameters.c
+++ parameters.c
@@ -78,14 +78,10 @@ avg_audio_energy(sample *buf, u_int32 samples, u_int32 channels)
                 e1 = max(e1, e2);
                 samples /= channels; /* No. of samples to no. of sampling intervals */
         }
-        /* This is the wrong way around - it should be:
-         *       e1 * (samples / step)
-         * Unfortunately all the code seems to work with this incorrect 
-         * scaling for the time being.  There is not enough time
-         * to fix this now and get everything working again, but
-         * someone should...
+
+        /* Return mean sampled energy:
+         * no. of sampling points = samples/step;
          */
-        
         return (u_int16)(e1*STEP/samples);
 }
 
