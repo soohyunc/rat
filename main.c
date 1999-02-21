87ed78338d057eec5378c1db901e5e8602c02ace
ucaccsp <ucaccsp@bc8abe95-590b-0410-a0f3-dc9ed9a1a8fa>
1999-02-21 11:49:57 +0000

    Cleanup some of the conference bus stuff... the UI is now only processed when enabled, and the hearbeat works right now. Colin

--- main.c
+++ main.c
@@ -143,6 +143,7 @@ main(int argc, char *argv[])
 	do {
 		network_process_mbus(sp[0]);
 		mbus_heartbeat(sp[0]->mbus_engine, 1);
+		mbus_heartbeat(sp[0]->mbus_ui, 1);
 		usleep(20000);
 	} while (sp[0]->wait_on_startup);
 
@@ -260,12 +261,14 @@ main(int argc, char *argv[])
 			if (sp[i]->audio_device) ui_update_powermeters(sp[i], sp[i]->ms, elapsed_time);
                 	if (sp[i]->ui_on) {
 				tcl_process_events(sp[i]);
-				mbus_send(sp[i]->mbus_ui); mbus_retransmit(sp[i]->mbus_ui);
+				mbus_send(sp[i]->mbus_ui); 
+				mbus_recv(sp[i]->mbus_ui, (void *) sp[i]);
+				mbus_retransmit(sp[i]->mbus_ui);
+				mbus_heartbeat(sp[i]->mbus_ui, 10);
                 	}
-			mbus_retransmit(sp[i]->mbus_engine);
 			mbus_send(sp[i]->mbus_engine); 
 			mbus_recv(sp[i]->mbus_engine, (void *) sp[i]);
-			mbus_recv(sp[i]->mbus_ui    , (void *) sp[i]);
+			mbus_retransmit(sp[i]->mbus_engine);
 			mbus_heartbeat(sp[i]->mbus_engine, 10);
 
                         if (sp[i]->new_config != NULL) {
