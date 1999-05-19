e93cb7757ded16a62cf60f9665eca1ea494e47af
ucacoxh <ucacoxh@bc8abe95-590b-0410-a0f3-dc9ed9a1a8fa>
1999-05-19 17:55:59 +0000

    - Added some documentation to the top of this file since I am using it   for something else and a little info is helpful ;-)

--- sndfile.h
+++ sndfile.h
@@ -18,6 +18,20 @@
 extern "C" {
 #endif
 
+/* The interface below allows access to Sun .au files and MS .wav files.
+ *
+ * The decision about which format is used depends on the extension of
+ * the file name.
+ *
+ * The functions snd_{read,write}_open take a pointer to a pointer 
+ * of type struct s_snd_file.  This makes allocation/access more
+ * restrictive, but safer.
+ *
+ * NOTE: for snd_read_audio and snd_write_audio, buf_len is the
+ * product of the number of sampling intervals to be written and
+ * the number of channels.
+ */
+
 struct s_snd_file;
 
 int  snd_read_open (struct s_snd_file **sf, 
