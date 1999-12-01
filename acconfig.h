a194a4be876a6e785dbf49583465ae20967e8396
ucacoxh <ucacoxh@bc8abe95-590b-0410-a0f3-dc9ed9a1a8fa>
1999-12-01 14:43:27 +0000

    - Autoconf support files. - configure.in is almost empty.  Will need quite a lot of work to generate   suitable configure script. - Add make distclean to Makefile.in to clean up unwanted configure gubbins   (cache, log, status).

---
+++ acconfig.h
@@ -0,0 +1,56 @@
+/*
+ * Define this if you have a /dev/urandom which can supply good random numbers.
+ */
+#undef HAVE_DEV_URANDOM
+
+/*
+ * Define this if you want IPv6 support.
+ */
+#undef HAVE_IPv6
+
+/*
+ * Define this if your C library doesn't have vsnprintf.
+ */
+#undef NEED_VSNPRINTF
+
+/*
+ * If you don't have these types in <inttypes.h>, #define these to be
+ * the types you do have.
+ */
+#undef int16_t
+#undef int32_t
+#undef int64_t
+#undef int8_t
+#undef uint16_t
+#undef uint32_t
+#undef uint8_t
+
+/*
+ * Debugging:
+ * DEBUG: general debugging
+ * DEBUG_MEM: debug memory allocation
+ */
+#undef DEBUG
+#undef DEBUG_MEM
+
+/*
+ * #defines for operating system.
+ * THESE WANT TO GO AWAY!
+ * Any checks for a specific OS should be replaced by a check for
+ * a feature that OS supports or doesn't support.
+ */
+#undef SunOS
+#undef Solaris
+#undef Linux
+#undef HPUX
+
+/* Audio device relate */
+#undef HAVE_PCA_AUDIO
+#undef HAVE_LUIGI_AUDIO
+#undef HAVE_OSS_AUDIO
+
+@BOTTOM@
+
+#ifndef WORDS_BIGENDIAN
+#define WORDS_SMALLENDIAN
+#endif
