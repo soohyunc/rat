2be280f3591328c71cda81d842949bbefc9edee6
ucacoxh <ucacoxh@bc8abe95-590b-0410-a0f3-dc9ed9a1a8fa>
2000-01-13 20:19:20 +0000

    - Backwards compatible name mappings (iteration #2 :-).

---
+++ codec_compat.c
@@ -0,0 +1,48 @@
+/*
+ * FILE:    codec_compat.c
+ * AUTHORS: Orion Hodson
+ * 
+ * Copyright (c) 1998-2000 University College London
+ * All rights reserved.
+ *
+ */
+
+#include <config_unix.h>
+#include <config_win32.h>
+
+#include "codec_compat.h"
+
+/* Backward compatibility name mapping for earlier MBONE audio applications */
+
+struct s_compat_entry {
+        const char *newname;
+        const char *oldname;
+};
+
+static struct s_compat_entry name_map[] = {
+        {"PCMU-8K-MONO", "pcm"},
+        {"PCMU-8K-MONO", "pcmu"},
+        {"PCMU-8K-MONO", "ulaw"},
+        {"PCMA-8K-MONO", "pcma"},
+        {"PCMU-8K-MONO", "alaw"},
+        {"DVI-8K-MONO",  "dvi"},
+        {"GSM-8K-MONO",  "gsm"},
+        {"LPC-8K-MONO",  "lpc"},
+        {"L16-8K-MONO",  "l16"}
+};
+
+#define NUM_COMPAT_NAMES sizeof(name_map)/sizeof(name_map[0])
+
+const char *
+codec_get_compatible_name(const char *name)
+{
+        uint16_t i;
+        if (name != NULL) {
+                for (i = 0; i < NUM_COMPAT_NAMES; i++) {
+                        if (!strcasecmp(name, name_map[i].oldname)) {
+                                return name_map[i].newname;
+                        }
+                }
+        }
+        return name;
+}
---
+++ codec_compat.h
@@ -0,0 +1,12 @@
+/*
+ * FILE:    codec_compat.h
+ * AUTHORS: Orion Hodson
+ * 
+ * Copyright (c) 1998-2000 University College London
+ * All rights reserved.
+ *
+ */
+
+/* Backward compatibility name mapping for earlier MBONE audio applications */
+/* Returns compatible name if found, or cname passed to it if not.          */
+const char *codec_get_compatible_name(const char *cname);
