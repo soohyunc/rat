601c8c38247ddf6ce9525286ee013fc8a2732aa1
piers <piers@bc8abe95-590b-0410-a0f3-dc9ed9a1a8fa>
2010-01-07 11:22:54 +0000

    Added a fix for when there is no available audio

--- auddev_oss.c
+++ auddev_oss.c
@@ -855,6 +855,8 @@ oss_audio_read(audio_desc_t ad, u_char *buf, int read_bytes)
         ioctl(devices[ad].audio_rfd, SNDCTL_DSP_GETISPACE, &info);
         available = min(info.bytes, read_bytes);
 
+        if (available == 0) return 0;
+
         errno = 0;
         read_len  = read(devices[ad].audio_rfd, (char *)buf, available);
         if (read_len < 0) {
